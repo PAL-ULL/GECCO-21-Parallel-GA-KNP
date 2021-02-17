542757f8822b8087e9cee7e30ec5e6c260a4d5aa
amarrerod <alemarrerodiaz@gmail.com>
2021-02-17 09:01:57 +0000

    feat: Includes allHV files

---
+++ script/table.sh
@@ -0,0 +1,24 @@
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/Uncorrelated/50 > Uncorrelated_50.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/InverseCorrelated/50 > InverseCorrelated_50.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/StronglyCorrelated/50 > StronglyCorrelated_50.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/SpannerStronglyCorrelated/50 > SpannerStronglyCorrelated_50.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/SubsetSum/50 > SubsetSum_50.txt
+
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/Uncorrelated/100 > Uncorrelated_100.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/InverseCorrelated/100 > InverseCorrelated_100.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/StronglyCorrelated/100 > StronglyCorrelated_100.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/SpannerStronglyCorrelated/100 > SpannerStronglyCorrelated_100.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/SubsetSum/100 > SubsetSum_100.txt
+
+
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/Uncorrelated/500 > Uncorrelated_500.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/InverseCorrelated/500 > InverseCorrelated_500.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/StronglyCorrelated/500 > StronglyCorrelated_500.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/SpannerStronglyCorrelated/500 > SpannerStronglyCorrelated_500.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/SubsetSum/500 > SubsetSum_500.txt
+
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/Uncorrelated/1000 > Uncorrelated_1000.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/InverseCorrelated/1000 > InverseCorrelated_1000.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/StronglyCorrelated/1000 > StronglyCorrelated_1000.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/SpannerStronglyCorrelated/1000 > SpannerStronglyCorrelated_1000.txt
+perl -w generate_tables.pl ParGA 30 /home/amarrero/Proyectos/CEC-2021-Parallel-GA-KNP/results/rayo/SubsetSum/1000 > SubsetSum_1000.txt
